1852bcadf693fbe97753b4c48daf4654f8313927
Samuel Marks <samuelmarks@gmail.com>
2018-11-16 16:14:05 +1100

    Added default_worker and default_db_url

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ extern crate envy;
 
 extern crate toml;
 extern crate dotenv;
+extern crate num_cpus;
 
 use std::io::Read;
+use std::env;
 
 use dotenv::dotenv;
 
@@ -24,4 +26,24 @@ pub fn acquire_config<T>(config_fname: String) -> T
             }
         },
     };
+}
+
+pub fn default_worker() -> usize {
+    num_cpus::get() * 8
+}
+
+pub fn default_db_url() -> String {
+    static DB_URL: &str = "DATABASE_URL";
+    match env::var_os(DB_URL) {
+        Some(db_url) => return db_url.to_string_lossy().to_string(),
+        None => {
+            match env::var_os("POSTGRES_USER") {
+                Some(user) => {
+                    let user_s = user.to_string_lossy();
+                    return format!("postgres://{}:{}@{}/{}", user_s, env::var("POSTGRES_PASSWORD").unwrap(), "localhost", env::var("POSTGRES_DB").unwrap_or((&*user_s).into()).to_string())
+                },
+                None => panic!(format!("{} must be defined", DB_URL))
+            }
+        }
+    }
 }
